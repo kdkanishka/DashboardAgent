249032a1a8ee6bb75fd70f219ca0923f444ac973
Kanishka Dilshan <kanishka.desilva@pagero.com>
2017-09-08 11:31:47 +0530

    Added support for frame processing

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"golang.org/x/net/websocket"
 	"log"
 	"strings"
+	"encoding/json"
 )
 
 func connectToWebSocket(ws_channel, quite_channel chan string) {
@@ -35,15 +36,53 @@ func connectToWebSocket(ws_channel, quite_channel chan string) {
 	}
 }
 
+func handleFrame(frame string, notification_channel chan Notification) {
+	if strings.Contains(frame, "InitialMonitoredService") {
+		var initialMonitoredService InitialMonitoredServiceResponse
+		error := json.Unmarshal([]byte(frame), &initialMonitoredService)
+		if error == nil {
+			fmt.Println("Successfully unmarshalled InitialMonitoredService")
+		}
+		for _, monitoredService := range initialMonitoredService.InitialMonitoredService.Items {
+			notification := Notification{
+				name:   monitoredService.ServiceName,
+				status: monitoredService.State,
+				item:   monitoredService,
+			}
+			notification_channel <- notification
+		}
+	} else if strings.Contains(frame, "UpdatedMonitoredService") {
+		var updated UpdatedMonitoredServiceResponse
+		error := json.Unmarshal([]byte(frame), &updated)
+		if error == nil {
+			fmt.Println("Successfully unmarshalled UpdatedMonitoredService")
+		}
+		for _, monitoredService := range updated.UpdatedMonitoredService.Items {
+			notification := Notification{
+				name:   monitoredService.ServiceName,
+				status: monitoredService.State,
+				item:   monitoredService,
+			}
+			notification_channel <- notification
+		}
+	}
+}
+
 func main() {
+	notifications := make(map[string]Notification)
+
 	ws_channel := make(chan string)
 	quite_channel := make(chan string)
+	notification_channel := make(chan Notification)
 	go connectToWebSocket(ws_channel, quite_channel)
 
 	for {
 		select {
 		case frame := <-ws_channel:
 			fmt.Printf("Frame receieved %s \n", frame)
+			go handleFrame(frame, notification_channel)
+		case notification := <-notification_channel:
+			fmt.Println(notification)
 		case sig_quite := <-quite_channel:
 			fmt.Printf("Exit signal received! %s\n", sig_quite)
 			log.Fatal("Exiting since error occured")
--- models.go
+++ models.go
@@ -4,23 +4,35 @@ type InitialMonitoredServiceResponse struct {
 	InitialMonitoredService InitialMonitoredService `json:"InitialMonitoredService"`
 }
 
+type UpdatedMonitoredServiceResponse struct {
+	UpdatedMonitoredService UpdatedMonitoredService `json:"UpdatedMonitoredService"`
+}
+
+type UpdatedMonitoredService struct {
+	Items []InitialMonitoredServiceItem `json:"items"`
+}
+
 type InitialMonitoredService struct {
 	Items []InitialMonitoredServiceItem `json:"items"`
 }
 
 type InitialMonitoredServiceItem struct {
-	ServiceName string `json:"serviceName"`
-	State string `json:"state"`
-	Alarms []Alarm `json:"alarms"`
+	ServiceName string  `json:"serviceName"`
+	State       string  `json:"state"`
+	Alarms      []Alarm `json:"alarms"`
 }
 
 type Alarm struct {
 	ResponsibleUnit string `json:"responsibleUnit"`
-	Label string `json:"label"`
-	AlarmType string `json:"alarmType"`
-	Origin string `json:"origin"`
-	Details string `json:"details"`
-	Severity string `json:severity`
+	Label           string `json:"label"`
+	AlarmType       string `json:"alarmType"`
+	Origin          string `json:"origin"`
+	Details         string `json:"details"`
+	Severity        string `json:severity`
 }
 
-
+type Notification struct {
+	name   string
+	item   InitialMonitoredServiceItem
+	status string
+}
