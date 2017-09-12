dcced026fadfcfd2f299df20c8e23d8235f50599
Kanishka Dilshan <kanishka.desilva@pagero.com>
2017-09-12 11:28:29 +0530

    Added service wrapper and README

---
+++ README.md
@@ -0,0 +1,3 @@
+Useful commands to deal with systemd 
+``journalctl -xe`` -> Show error log
+``sudo systemctl start dashboardagent.service`` manage service
--- utils/config.go
+++ utils/config.go
@@ -3,6 +3,7 @@ package utils
 import (
 	"encoding/json"
 	"io/ioutil"
+	"flag"
 )
 
 type Configuration struct {
@@ -10,9 +11,12 @@ type Configuration struct {
 }
 
 func Read() Configuration {
-	fileContent, error := ioutil.ReadFile("config.json")
+	flagFile := flag.String("config", "config.json", "Configuration file")
+	flag.Parse()
+
+	fileContent, error := ioutil.ReadFile(*flagFile)
 	if error != nil {
-		Log.Printf("Unable to open configuration file %s", error.Error())
+		Log.Printf("Unable to open configuration file %s File:%s\n", error.Error(), *flagFile)
 		panic("Unable to continue! since configuration file is not available")
 	}
 
